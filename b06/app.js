8494076127537f018cb8f104bd641d7083f16320
VanDuy <vanduy6814@gmail.com>
2026-06-08 21:21:14 +0700

    b06 init

---
+++ b06/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var createError = require('http-errors');
+
+require('dotenv').config()
+require("./src/apps/db/main.db")
+
+var indexRouter = require('./src/routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', indexRouter);
+
+app.use((req,res,next) => {
+    next(createError(404))
+})
+
+
+app.use((error , req , res , next) => {
+
+    console.log("status", error.status)
+    console.log("error", error.message)
+
+    return res.status(error.status || 500).json({
+        status: error.status || 500,
+        message : error.message
+    })
+})
+
+module.exports = app;
---
+++ b06/src/controllers/user.controller.js
@@ -0,0 +1,68 @@
+
+const { findAll, findById, createUser, deleteUser, editUser } = require("../services/user.services")
+const { ErrorCustom, BAD_REQUEST_ERROR, UNAUTHORIZED_ERROR } = require("../apps/core/error.res")
+
+const getAllUser = async  (req, res) => {
+    let data =  await findAll()
+    res.json({
+        message: "Get All User",
+        data
+    })
+}
+
+// xoa 
+// getOne
+
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    
+    const data = await findById(id);
+
+    res.json({
+        message: "Get User By Id",
+        data
+    });
+}
+
+const addUser = async (req, res) => {
+    console.log(req.body)
+    await createUser(req.body)
+    res.json({
+        message: "Add User",
+      
+    })
+}
+
+
+const updateUser = async (req, res) => {
+    let { id } = req.params;
+    let userData = req.body;
+
+    if (!id) throw new BAD_REQUEST_ERROR()
+    // check id có phải là id của mongdb hay không
+
+    const idFind = await findById(id);
+    if (!idFind) throw new BAD_REQUEST_ERROR("Not found id")
+
+    
+
+    let isOk = await editUser(id, userData);
+    res.json({
+        message: "Edit User",
+        metaData: {}
+    })
+}
+
+
+const removeUser = async (req, res) => {
+    const { id } = req.params;
+
+    const user = await deleteUser(id);
+
+    res.json({
+        message: "Delete User Success",
+        data: user
+    });
+}
+
+module.exports = { updateUser  , getAllUser, addUser, getUserById, removeUser }
